f17fe3244523530e1903ea4886d474caf84ab204
Sandun <sandunkdissanayake@gmail.com>
2021-06-24 21:32:15 +0530

    submit assginment

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
     return redirect('/payment/split');
 });
 
